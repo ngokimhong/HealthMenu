db7317e88e78fcea530e96e284fe0c78ec5045ad
NgoKimHong <ngokimhong031296@gmail.com>
2018-10-23 23:18:19 +0700

    commit 1

---
+++ app/src/main/java/com/example/taminhhieu/healthmenu/LoadingActivity.java
@@ -0,0 +1,51 @@
+package com.example.taminhhieu.healthmenu;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        Button mShowLoginDialog = findViewById(R.id.btnShowDialog);
+        mShowLoginDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(LoadingActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+                final EditText mId = mView.findViewById(R.id.txtID);
+                final EditText mPassword = mView.findViewById(R.id.txtPassword);
+                Button mLogin = mView.findViewById(R.id.btnLoginD);
+
+                mLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!mId.getText().toString().isEmpty() && !mPassword.getText().toString().isEmpty()){
+                            Toast.makeText(LoadingActivity.this,
+                                    R.string.success_login_msg,
+                                    Toast.LENGTH_SHORT).show();
+                            }
+                        else{
+                            Toast.makeText(LoadingActivity.this,
+                                    R.string.error_login_msg,
+                                    Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+            }
+        });
+    }
+}
